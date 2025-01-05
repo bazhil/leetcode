d216c189b8f0aa7c082d9865797aec181db2492c
ilya <i-bazh@mail.ru>
2025-01-05 17:03:49 +0300

    https://leetcode.com/problems/median-of-two-sorted-arrays/

--- 4_median_of_two_sorted_arrays.py
+++ 4_median_of_two_sorted_arrays.py
@@ -1,10 +1,23 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
-        # Simple way (probably not right way, but leetcode tests is ok)
-        m1 = reduce(lambda x, y: x + y, nums1) / len(nums1)
-        m2 = reduce(lambda x, y: x + y, nums2) / len(nums2)
+        if nums1 and nums2:
+            median = self.find_median(nums1 + nums2)
+        elif nums1 and not nums2:
+            median = self.find_median(nums1)
+        elif nums2 and not nums1:
+            median = self.find_median(nums2)
 
-        return (m1 + m2) / 2
+        return median
 
-        #TODO: find better design (The overall run time complexity should be O(log (m+n)).)
+    def find_median(self, arr):
+        n = len(arr)
+
+        # First we sort the array
+        arr.sort()
+
+        # check for even case
+        if n % 2 != 0:
+            return arr[n // 2]
+
+        return (arr[(n - 1) // 2] + arr[n // 2]) / 2.0
