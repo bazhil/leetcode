3c04ba85032755f22a819d50d491e658aae6608e
ilya <ilya.bazhenov@raftds.com>
2025-03-30 13:28:55 +0300

    https://leetcode.com/problems/valid-sudoku/description/

--- 36_valid_sudoku.py
+++ 36_valid_sudoku.py
@@ -1,31 +1,18 @@
 class Solution:
-
-    def get_sudoku_butch(self, board):
-        for row in range(0, 9, 3):
-            for col in range(0, 9, 3):
-                square = []
-                for i in range(3):
-                    square.extend(board[row + i][col:col + 3])
-                yield square
-
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        if len(board) != 9:
-            return False
-        for line in board:
-            if len(line) != 9:
-                return False
-            for el in line:
-                if el != '.' and not el.isdigit():
-                    return False
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r/3, c/3)
 
-        for lst in self.get_sudoku_butch(board):
-            for el in lst:
-                if el != '.' and not el.isdigit():
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or board[r][c] in rows[c] or board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-            nums = [el for el in lst if el.isdigit()]
-            if not nums:
-                return False
-            if len(nums) > len(set(nums)):
-                return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
 
         return True
+
